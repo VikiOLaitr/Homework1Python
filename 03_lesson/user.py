7cddccf045b34e6fc53155d3d51de17830c2445a
VikiOLaitr <Ruri19933010@yandex.ru>
2024-09-26 12:27:38 +0300

    git add -a

--- 03_lesson/user.py
+++ 03_lesson/user.py
@@ -3,11 +3,11 @@ class User:
         self.first_name = first_name
         self.last_name = last_name
 
-        def get_first(self):
-            return self.first_name
+    def get_first(self):
+        return self.first_name
 
-        def get_last(self):
-            return self.last_name
+    def get_last(self):
+        return self.last_name
 
-        def get_first_last(self):
-            return f"Имя: {self.first_name}, Фамилия: {self.last_name}"
+    def get_first_last(self):
+        return f"Имя: {self.first_name}, Фамилия: {self.last_name}"
