f9737e8aaa734deb82e17df9a60c5ff3e5386ee0
VikiOLaitr <Ruri19933010@yandex.ru>
2024-10-16 18:34:08 +0300

    git add -a

--- 06_lesson/Image.py
+++ 06_lesson/Image.py
@@ -8,10 +8,8 @@ driver = webdriver.Chrome(
     service=ChromeService(ChromeDriverManager().install()))
 
 driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
-waiter = WebDriverWait(driver, 20)
-waiter.until(
-    EC.visibility_of_all_elements_located("img")
-)
-src = driver.find_element(By.CSS_SELECTOR, "img#award").get_attribute("src")
+waiter = WebDriverWait(driver, 30).until(
+    EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div#image-container.col-12.py-2 img#landscape")))
+src = driver.find_element(By.CSS_SELECTOR, "#award").get_attribute("src")
 print(src)
 driver.quit()
